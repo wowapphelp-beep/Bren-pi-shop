8290709c5e576b040d41224e5017034b16b59f7c
wowapphelp-beep <wowapphelp@gmail.com>
2026-08-18 09:36:00 +0530

    Update Supabase publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://trdxjxjiknrbbrgitpbv.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "अपनी_publishable_key_यहाँ_रखें";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6mJyEsoAWN2a94QobAFWuA_zb2B0crM";
